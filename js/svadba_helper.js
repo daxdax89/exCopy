cfc4024ae216eb60a7f0b7d0cf6d1d2cbd20c0bd
daxdax89 <daxdax89@msn.com>
2017-11-07 02:27:43 +0100

    Edited some LAF

--- js/svadba_helper.js
+++ js/svadba_helper.js
@@ -1,13 +1,13 @@
 function helper(){
 	var div = '<div id="chatOSSvadbaHelper">' +
 		'<div id="helperHeader">' +
-			'<h5>' + __('Вас приветствует расширение ChatOS') + '</h5>' + 
+			'<h5>' + __('Welcome to ExodiousUI') + '</h5>' + 
 			'<a class="closeHelper"></a>' + 
 		'</div>' +
 		'<div id="helperBody">' + 
-			'<a href="https://www.svadba.com/chat/#" class="buttons">' + __('Перейти в чат') + '</a>' + 
-			'<a href="https://chatoptimizer.com/?p=148" class="buttons" target="_blank">' + __('Справка') + '</a>' + 
-			'<a href="https://account.chatoptimizer.com/customer/topup" class="buttons" target="_blank">' + __('Пополнить баланс') + '</a>' + 
+			'<a href="https://www.svadba.com/chat/#" class="buttons">' + __('Open Chat') + '</a>' + 
+			'<a href="https://chatoptimizer.com/?p=148" class="buttons" target="_blank">' + __('Help') + '</a>' + 
+			'<a href="https://account.chatoptimizer.com/customer/topup" class="buttons" target="_blank">' + __('Add Credits') + '</a>' + 
 		'</div>' + 
 	'</div>';
 
